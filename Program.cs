dae51a294fbcce00516d45ce44a049e489d51ce7
ViscountFish <waldencifuentes85@gmail.com>
2024-08-28 10:54:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my calculator");
+            Console.WriteLine();
+
+            Console.Write("Enter Your first number: ");
+            string firstNumberInput = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(firstNumberInput);
+
+            Console.Write("Enter Your your operator: "); // + - / *
+            string operatorInput = Console.ReadLine();
+
+            Console.Write("Enter Your first number: ");
+            string secondNumberInput = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(secondNumberInput);
+
+            double result = 0.0; 
+            
+            if (operatorInput.Contains("+"))
+            {
+                result = firstNumber + secondNumber;
+            }
+            else if (operatorInput.Contains("-"))
+            {
+                result = firstNumber - secondNumber;
+            }
+            else if (operatorInput.Contains("/"))
+            {
+                result = firstNumber / secondNumber;
+            }
+            else if (operatorInput.Contains("*"))
+            {
+                result = firstNumber * secondNumber;
+            }
+
+            Console.WriteLine("The result of " + firstNumber + " " + operatorInput + " " + secondNumber + " is: " + result);
+
+            Console.ReadLine ();
+        }
+    }
+}
